484daee7f254fad7dea3c2dd34461da994b221c5
Issam Ramdani <socrates98@protonmai.com>
2025-01-08 12:21:39 +0100

    hecho

--- catalog/views.py
+++ catalog/views.py
@@ -19,12 +19,17 @@ def index(request):
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
+    
+    num_visits = request.session.get('visitas', 0)
+    num_visits += 1
+    request.session['visitas'] = num_visits
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits
     }
 
     # Render the HTML template index.html with the data in the context variable
@@ -112,3 +117,9 @@ lista = []
     return render(request, "list.html", {"page_obj": page_obj})
 
 """
+
+
+
+
+def probandoSession(request):
+    pass
