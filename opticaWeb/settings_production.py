90f543b0acbf23656a8a5e178e6ed92cd3f7471a
Issam Ramdani <73023437+slic23@users.noreply.github.com>
2025-02-06 18:50:58 +0100

    Update settings_production.py

--- opticaWeb/settings_production.py
+++ opticaWeb/settings_production.py
@@ -4,7 +4,7 @@ DEBUG = False
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "HOST": config("PGHOST"),
+        "HOST": config("POSTGRES_HOST"),
         "PORT": 5432,
         "NAME": config("POSTGRES_DATABASE"),
         "USER": config("POSTGRES_USER"),
