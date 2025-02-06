62e715590b6a8415f7d6df057745fb24c73fff85
Issam Ramdani <73023437+slic23@users.noreply.github.com>
2025-02-06 19:00:00 +0100

    Update settings_production.py

--- opticaWeb/settings_production.py
+++ opticaWeb/settings_production.py
@@ -6,8 +6,8 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "HOST": "ep-purple-poetry-a2c68m2j-pooler.eu-central-1.aws.neon.tech",
         "PORT": 5432,
-        "NAME": config("POSTGRES_DATABASE"),
-        "USER": config("POSTGRES_USER"),
-        "PASSWORD": config("POSTGRES_PASSWORD")
+        "NAME": ,
+        "USER": "neondb",
+        "PASSWORD": "npg_mjDnbxXiI06z"
     }
 }
