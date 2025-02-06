97dd7a01b76ccb97db6769d959192a4c27c7d131
Issam Ramdani <73023437+slic23@users.noreply.github.com>
2025-02-06 18:38:44 +0100

    Update settings

--- opticaWeb/settings_production.py
+++ opticaWeb/settings_production.py
@@ -1,13 +1,13 @@
 from decouple import config, Csv
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
-DEBUG = True
+ALLOWED_HOSTS = ['.vercel.app']
+DEBUG = False
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "HOST": config("POSTGRES__POSTGRES_HOST"),
+        "HOST": config("POSTGRES_HOST"),
         "PORT": 5432,
-        "NAME": config("POSTGRES__POSTGRES_DATABASE"),
-        "USER": config("POSTGRES__POSTGRES_USER"),
-        "PASSWORD": config("POSTGRES__POSTGRES_PASSWORD")
+        "NAME": config("POSTGRES_DATABASE"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD")
     }
 }
